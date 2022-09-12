b532042e2180f62045e35e6fc2e49bfaa4ac5d68
Pramod_Bhavikhatte <108565873+bhavikhattepramod@users.noreply.github.com>
2022-09-12 18:15:06 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:application/createuser.dart';
+import 'package:application/login.dart';
+import 'package:flutter/material.dart';
+
+import 'splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Create_User());
+  }
+}
